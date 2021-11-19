fa2da6aa3f4616754028fe4353db9a5edb3e7c6d
sanghyun-lee2 <h983019@naver.com>
2021-11-19 14:51:21 +0900

    테이블 이름 변경 반영

--- happyhouse_frontend/src/components/house/HouseSearchBar.vue
+++ happyhouse_frontend/src/components/house/HouseSearchBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <b-row class="mt-4 mb-4 text-center">
-    <!-- <b-col class="sm-3">
+    <b-row class="mt-4 mb-4 text-center">
+        <!-- <b-col class="sm-3">
             <b-form-input
                 v-model.trim="dongCode"
                 placeholder="동코드 입력...(예 : 11110)"
@@ -12,28 +12,28 @@
                 >검색</b-button
             >
         </b-col> -->
-    <b-col class="sm-3">
-      <b-form-select
-        v-model="sidoCode"
-        :options="sidos"
-        @change="gugunList"
-      ></b-form-select>
-    </b-col>
-    <b-col class="sm-3">
-      <b-form-select
-        v-model="gugunCode"
-        :options="guguns"
-        @change="dongList"
-      ></b-form-select>
-    </b-col>
-    <b-col class="sm-3">
-      <b-form-select
-        v-model="dongCode"
-        :options="dongs"
-        @change="searchHouse"
-      ></b-form-select>
-    </b-col>
-  </b-row>
+        <b-col class="sm-3">
+            <b-form-select
+                v-model="sidoCode"
+                :options="sidos"
+                @change="gugunList"
+            ></b-form-select>
+        </b-col>
+        <b-col class="sm-3">
+            <b-form-select
+                v-model="gugunCode"
+                :options="guguns"
+                @change="dongList"
+            ></b-form-select>
+        </b-col>
+        <b-col class="sm-3">
+            <b-form-select
+                v-model="dongCode"
+                :options="dongs"
+                @change="searchHouse"
+            ></b-form-select>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
@@ -82,7 +82,7 @@ export default {
         ]),
 
         gugunList() {
-            // console.log(this.sidoCode);
+            console.log(this.sidoCode);
             this.CLEAR_GUGUN_LIST();
             this.gugunCode = null;
             this.CLEAR_DONG_LIST();
