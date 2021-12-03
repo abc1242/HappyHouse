9144ae79f3a9a553458ec80f8e4dfc7b133efbfa
sanghyun-lee2 <h983019@naver.com>
2021-12-03 17:37:14 +0900

    Heroku 배포를 위한 환경설정 변경!

--- happyhouse_frontend/src/config/index.js
+++ happyhouse_frontend/src/config/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:9999/vue";
+const API_BASE_URL = "https://shlee-happyhouse.herokuapp.com/";
 const API_NEWS_URL = "https://openapi.naver.com/v1/search/news.json";
 
 export { API_BASE_URL, API_NEWS_URL };
